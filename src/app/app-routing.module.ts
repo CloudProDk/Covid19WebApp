5a6478f39fddf81f2316eaf0beddfbc077af9be2
serubi <serubi@live.com>
2020-05-14 16:07:54 +0200

    Fix merch routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { Routes, RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { TrackerComponent } from './components/tracker/tracker.component';
 import { QuizComponent } from './components/quiz/quiz.component';
+import { MerchComponent } from './components/merch/merch.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'tracker', pathMatch: 'full'},
   { path: 'tracker', component: TrackerComponent },
+  { path: 'merch', component: MerchComponent},
   { path: 'quiz', component: QuizComponent}
 ];
 
