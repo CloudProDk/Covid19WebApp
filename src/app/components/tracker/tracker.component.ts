dd7646f02a9563027e1e114efdecb6b16b8b817d
Thom9521 <Thom9521@edu.easj.dk>
2020-05-15 16:10:15 +0200

    Global highlights

--- src/app/components/tracker/tracker.component.ts
+++ src/app/components/tracker/tracker.component.ts
@@ -2,19 +2,27 @@ import { Component, OnInit, Inject } from "@angular/core";
 import { TrackerService } from "../../services/tracker/tracker.service";
 import { Country } from "../../models/country";
 import { NumberFormatPipe } from "../../shared/number-format.pipe";
-import { DecimalPipe } from '@angular/common';
-import { MatDatepickerInputEvent } from '@angular/material';
+import { DecimalPipe } from "@angular/common";
+import { MatDatepickerInputEvent } from "@angular/material";
 
 @Component({
   selector: "app-tracker",
   templateUrl: "./tracker.component.html",
   styleUrls: ["./tracker.component.scss"],
-  providers: [ NumberFormatPipe, DecimalPipe ]
+  providers: [NumberFormatPipe, DecimalPipe],
 })
 export class TrackerComponent implements OnInit {
   countries: any = [];
   country: any = {};
-  displayedColumns: string[] = ['country', 'totalCases', 'activeCases', 'newCases', 'totalDeaths', 'newDeaths', 'totalRecovered'];
+  displayedColumns: string[] = [
+    "country",
+    "totalCases",
+    "activeCases",
+    "newCases",
+    "totalDeaths",
+    "newDeaths",
+    "totalRecovered",
+  ];
   todayDate: Date;
   minDate: Date;
   maxDate: Date;
@@ -27,9 +35,21 @@ export class TrackerComponent implements OnInit {
   totalDeaths: number = 0;
   totalNewDeaths: number = 0;
   totalRecovered: number = 0;
+  totalPopulation: number = 0;
+  totalCritical: number = 0;
 
-  constructor(private trackerSvc: TrackerService, private numberFormat: NumberFormatPipe, private decimalFormat: DecimalPipe) {
-    const today = new Date(Date.UTC(new Date().getFullYear(), new Date().getMonth(), new Date().getDate()));
+  constructor(
+    private trackerSvc: TrackerService,
+    private numberFormat: NumberFormatPipe,
+    private decimalFormat: DecimalPipe
+  ) {
+    const today = new Date(
+      Date.UTC(
+        new Date().getFullYear(),
+        new Date().getMonth(),
+        new Date().getDate()
+      )
+    );
     this.todayDate = today;
     this.minDate = today;
     this.maxDate = today;
@@ -56,7 +76,7 @@ export class TrackerComponent implements OnInit {
       scroll(0, 0);
       this.selectedDateIndex = 0;
 
-      if(this.country.timeline && Array.isArray(this.country.timeline)) {
+      if (this.country.timeline && Array.isArray(this.country.timeline)) {
         this.minDate = new Date(this.country.timeline.slice(-1)[0].date);
         this.dateToIndexDictionary = {}; // Reset
 
@@ -71,32 +91,58 @@ export class TrackerComponent implements OnInit {
   }
 
   getCountryDK() {
-    this.getCountry('DK');
+    this.getCountry("DK");
     // this.trackerSvc.getCountry("DK").subscribe((data) => {
     //   this.country = data.data;
     // });
   }
+  getGlobalCountry() {
+    this.country.timeline = undefined;
+    this.country.name = "Global";
+    this.country.code = "EARTH";
+    this.country.latest_data.confirmed = this.totalConfirmed;
+    this.country.latest_data.recovered = this.totalRecovered;
+    this.country.population = this.totalPopulation;
+    this.country.latest_data.deaths = this.totalDeaths;
+    this.country.latest_data.critical = this.totalCritical;
+    this.country.today.deaths = this.totalNewDeaths;
+    this.country.today.confirmed = this.totalNewCases;
+  }
 
   updateDate(event: MatDatepickerInputEvent<Date>) {
-    const updateDate = new Date(Date.UTC(event.value.getFullYear(), event.value.getMonth(), event.value.getDate()));
+    const updateDate = new Date(
+      Date.UTC(
+        event.value.getFullYear(),
+        event.value.getMonth(),
+        event.value.getDate()
+      )
+    );
     this.selectedDate = updateDate;
-    this.selectedDateIndex = this.dateToIndexDictionary[updateDate.toJSON().slice(0,10)];
+    this.selectedDateIndex = this.dateToIndexDictionary[
+      updateDate.toJSON().slice(0, 10)
+    ];
   }
 
   getEmojiUnicode(country: string) {
-    if(!country)
-      return country;
+    if (!country) return country;
 
-    if(country === 'EARTH')
-      return '1f30e';
-    
-    return twemoji.convert.toCodePoint(country.toUpperCase().replace(/./g, char => String.fromCodePoint(char.charCodeAt(0)+127397)));
+    if (country === "EARTH") return "1f30e";
+
+    return twemoji.convert.toCodePoint(
+      country
+        .toUpperCase()
+        .replace(/./g, (char) =>
+          String.fromCodePoint(char.charCodeAt(0) + 127397)
+        )
+    );
   }
 
   formatNumber(number: Number, decimals: Number = 1) {
-    return number >= 10000 ? this.numberFormat.transform(number, decimals) : this.decimalFormat.transform(number, '1.');
+    return number >= 10000
+      ? this.numberFormat.transform(number, decimals)
+      : this.decimalFormat.transform(number, "1.");
   }
-  
+
   getTotalData() {
     this.trackerSvc.getCountries().subscribe((data) => {
       for (let i = 0; i < data.data.length; i++) {
@@ -106,6 +152,8 @@ export class TrackerComponent implements OnInit {
         this.totalDeaths += data.data[i].latest_data.deaths;
         this.totalNewDeaths += data.data[i].today.deaths;
         this.totalRecovered += data.data[i].latest_data.recovered;
+        this.totalPopulation += data.data[i].population;
+        this.totalCritical += data.data[i].latest_data.critical;
       }
     });
   }
